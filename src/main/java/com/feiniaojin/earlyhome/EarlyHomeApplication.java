ad1be2571bb3f00e10db992179650698b637b5d5
feiniaojin <943868899@qq.com>
2020-04-14 20:03:32 +0800

    init

---
+++ src/main/java/com/feiniaojin/earlyhome/EarlyHomeApplication.java
@@ -0,0 +1,13 @@
+package com.feiniaojin.earlyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlyHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feiniaojin/earlyhome/EarlyHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feiniaojin.earlyhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarlyHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
